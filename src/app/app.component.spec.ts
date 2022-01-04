19ac909b72fefbcd490213b22e03e60a0cc96adb
ashikfec01 <kaziashik999@gmail.com>
2022-01-04 18:57:53 +0600

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'toassen-ng'`, () => {
+  it(`should have as title 'toaseen-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('toassen-ng');
+    expect(app.title).toEqual('toaseen-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('toassen-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('toaseen-ng app is running!');
   });
 });
